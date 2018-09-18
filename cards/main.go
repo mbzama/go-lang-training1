75808f4d306e3832d221507a8eb1382402027499
mbzama <zamamb@gmail.com>
2018-09-18 13:49:41 +0530

    Cards project

---
+++ cards/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	//cards := newDeck()
+	//cards.saveToFile("my_cards")
+
+	cards := newDeckFromFile("my_cards")
+	cards.shuffle()
+	cards.print()
+}
--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type color string
+
+func (c color) describe(description string) string {
+	return string(c) + " " + description
+}
 
 func main() {
-    fmt.Printf("hello, world\n")
+	//c := color("Red")
+	// fmt.Println(c.describe("is an awesome color"))
+
+	fmt.Println([]byte("a b c"))
 }
